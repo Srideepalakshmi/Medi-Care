0077ab3c3c7a6840c5a462bf6b5e1f75f0930eae
SRIDEEPALAKSHMI S <166732707+Srideepalakshmi@users.noreply.github.com>
2025-04-11 08:40:06 +0530

    skin_app.py

--- flask-backend/skin_app.py
+++ flask-backend/skin_app.py
@@ -5,14 +5,14 @@ import joblib
 app = Flask(__name__)
 
 # === Load CSV file ===
-df = pd.read_csv("Skin_Infections_Analyzer.csv")
+df = pd.read_csv("flask-backend/Skin_Infections_Analyzer.csv")
 
 # === Extract symptom columns ===
 symptom_columns = [col for col in df.columns if col not in ["Age", "Gender", "class", "Disease", "Suggestion"]]
 
 # === Load model and vectorizer ===
-model = joblib.load("skin_model.pkl")
-vectorizer = joblib.load("skin_vectorizer.pkl")
+model = joblib.load("flask-backend/skin_model.pkl")
+vectorizer = joblib.load("flask-backend/skin_vectorizer.pkl")
 
 
 html_template = """
@@ -1115,4 +1115,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=50010)
+    port = int(os.environ.get("PORT", 5009))  # Render provides PORT dynamically
+    app.run(host="0.0.0.0", port=port, debug=True)
