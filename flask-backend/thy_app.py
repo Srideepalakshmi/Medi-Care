dce5b282bfd5a74e5e6d0b168bd91ee0553f3a7e
SRIDEEPALAKSHMI S <166732707+Srideepalakshmi@users.noreply.github.com>
2025-04-11 08:41:28 +0530

    thy_app.py

--- flask-backend/thy_app.py
+++ flask-backend/thy_app.py
@@ -5,14 +5,14 @@ import joblib
 app = Flask(__name__)
 
 # === Load CSV file ===
-df = pd.read_csv("Thyroid_Disorders_Analyzer.csv")
+df = pd.read_csv("flask-backend/Thyroid_Disorders_Analyzer.csv")
 
 # === Extract symptom columns ===
 symptom_columns = [col for col in df.columns if col not in ["Age", "Gender", "class", "Disease", "Suggestion"]]
 
 # === Load model and vectorizer ===
-model = joblib.load("thy_model.pkl")
-vectorizer = joblib.load("thy_vectorizer.pkl")
+model = joblib.load("flask-backend/thy_model.pkl")
+vectorizer = joblib.load("flask-backend/thy_vectorizer.pkl")
 
 
 html_template = """
@@ -1115,4 +1115,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5005)
+    port = int(os.environ.get("PORT", 50010))  # Render provides PORT dynamically
+    app.run(host="0.0.0.0", port=port, debug=True)
