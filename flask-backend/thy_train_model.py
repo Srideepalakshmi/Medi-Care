c04667a27794477be731c1d05457f99f957d560d
SRIDEEPALAKSHMI S <166732707+Srideepalakshmi@users.noreply.github.com>
2025-04-11 08:42:06 +0530

    thy_train_model.py

--- flask-backend/thy_train_model.py
+++ flask-backend/thy_train_model.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # === Load dataset ===
-df = pd.read_csv("Thyroid_Disorders_Analyzer.csv")
+df = pd.read_csv("flask-backend/Thyroid_Disorders_Analyzer.csv")
 
 # === Drop missing values (if any) ===
 df.dropna(inplace=True)
@@ -36,7 +36,7 @@ model = RandomForestClassifier(n_estimators=100, class_weight="balanced", random
 model.fit(X_train, y_train)
 
 # === Save model and vectorizer ===
-joblib.dump(model, "thy_model.pkl")
-joblib.dump(vectorizer, "thy_vectorizer.pkl")
+joblib.dump(model, "flask-backend/thy_model.pkl")
+joblib.dump(vectorizer, "flask-backend/thy_vectorizer.pkl")
 
 print("✅ Model and vectorizer saved successfully!")
